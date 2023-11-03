c5ff6526cac4f307ab67750646a02e0263645267
Vasiliy Tsarev <90621078+VTsarevNSU@users.noreply.github.com>
2023-11-03 11:13:27 +0700

    небольшие изменения
    почистил код и комментарии + добавил картинки, по сути подготовил к PRу просто

--- app/src/main/java/ru/nsu/ccfit/tsd/pinmap/MainActivity.kt
+++ app/src/main/java/ru/nsu/ccfit/tsd/pinmap/MainActivity.kt
@@ -3,29 +3,21 @@ package ru.nsu.ccfit.tsd.pinmap
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Canvas
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
-import org.osmdroid.api.IGeoPoint
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.ItemizedIconOverlay
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.Overlay
-import org.osmdroid.views.overlay.OverlayItem
-import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow
 import ru.nsu.ccfit.tsd.pinmap.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,108 +29,100 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Log.d("idiot", "oncreate called")
-
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         val res = this.resources
-        val pinImage = ResourcesCompat.getDrawable(res, R.drawable.real_pin, null)
+        val pinImage = ResourcesCompat.getDrawable(res, R.drawable.pin_marker, null)
 
-        // рекомендуется сначала получить разрешения
-        // ACCESS_FINE_LOCATION нужен чтобы показывать текущее местоположение
-        // WRITE_EXTERNAL_STORAGE нужен чтобы показывать карту
+        /* рекомендуется сначала получить разрешения
+         ACCESS_FINE_LOCATION нужен чтобы показывать текущее местоположение
+         WRITE_EXTERNAL_STORAGE нужен чтобы показывать карту */
         val permissions: Array<String>
         permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION)
         requestPermissionsIfNecessary(permissions)
 
-        //проинициализировать кнопку добавления маркера
+        /* проинициализировать кнопку добавления маркера */
         binding.addMarkerFab.setOnClickListener { view ->
-            //Toast.makeText(view.context, "you add marker", Toast.LENGTH_SHORT).show()
-            view.visibility = View.GONE // убираем кнопку на время
-            binding.confirmMarkerFab.visibility = View.VISIBLE // зато показываем кнопку-подтверждение
-            binding.cancelMarkerFab.visibility = View.VISIBLE // и кнопку-отмену
+            view.visibility = View.GONE /* убираем кнопку на время */
+            binding.confirmMarkerFab.visibility = View.VISIBLE /* зато показываем кнопку-подтверждение */
+            binding.cancelMarkerFab.visibility = View.VISIBLE /* и кнопку-отмену */
         }
 
-        //проинициализировать кнопку подтверждения добавления маркера
+        /* проинициализировать кнопку подтверждения добавления маркера */
         binding.confirmMarkerFab.setOnClickListener { view ->
-            //Toast.makeText(view.context, "you confirm marker", Toast.LENGTH_SHORT).show()
-            Toast.makeText(view.context, map.overlays.size.toString(), Toast.LENGTH_SHORT).show()
-            binding.addMarkerFab.visibility = View.VISIBLE // снова показываем кнопку добавления маркера
-            view.visibility = View.GONE // а эту снова убираем
-            binding.cancelMarkerFab.visibility = View.GONE // и cancel тоже
+            binding.addMarkerFab.visibility = View.VISIBLE /* снова показываем кнопку добавления маркера */
+            view.visibility = View.GONE /* а эту снова убираем */
+            binding.cancelMarkerFab.visibility = View.GONE /* и cancel тоже */
 
-            //TODO создать маркер тут не получается, т.е. случается onCreate() у мэйн активити, и всё обнуляется
-            //TODO на это можно забить, если в конструкторе добавить маркер в базу, а в onCreate()
-            //TODO вытащить его из базы
+            /* создать перманентный маркер тут не получается, т.к. случается onCreate() у мэйн активити, и всё обнуляется
+            на это можно забить, если в конструкторе добавить маркер в базу, а в onCreate() вытащить его из базы
+            текущий код просто покажет маркер за момент до того как откроется активити по созданию маркера */
+            /*todo заменить активити фрагментами чтобы onCreate() не вызывать каждый раз;
+            todo либо можно как-то поменять поведение активити при запуске интента, не уверен
+            todo использовать кэширование чтобы не кидать кучу запросов каждый раз; однако это если остальное успеем*/
             val testMarker = PinMarker(binding.map)
             testMarker.position = map.mapCenter as GeoPoint
-            testMarker.icon = pinImage //используем свою картинку
+            testMarker.icon = pinImage /* используем свою картинку */
             testMarker.setAnchor(
                 Marker.ANCHOR_CENTER,
                 Marker.ANCHOR_BOTTOM
-            ) // это вроде просто указывает, какое место
-            // на картинке соответствует указанной координате
+            )/* это указание, какое место
+            на картинке соответствует указанной координате
+            в нашем случае это центр низа, т.е. кончик пина */
             binding.map.overlays.add(testMarker)
             binding.map.invalidate()
 
+            /* вызываем активити с созданием маркера */
             val context = map.context
             val intent = Intent(context, PinCreateActivity::class.java)
-            context.startActivity(intent) // вызываем активити с созданием маркера
+            context.startActivity(intent)
         }
 
-        //проинициализировать кнопку отмены добавления маркера
+        /* проинициализировать кнопку отмены добавления маркера */
         binding.cancelMarkerFab.setOnClickListener { view ->
             Toast.makeText(view.context, "Создание маркера отменено!", Toast.LENGTH_SHORT).show()
-            binding.addMarkerFab.visibility = View.VISIBLE // снова показываем кнопку добавления маркера
-            view.visibility = View.GONE // а эту снова убираем
-            binding.confirmMarkerFab.visibility = View.GONE // и confirm тоже
+            binding.addMarkerFab.visibility = View.VISIBLE /* снова показываем кнопку добавления маркера */
+            view.visibility = View.GONE /* а эту снова убираем */
+            binding.confirmMarkerFab.visibility = View.GONE /* и confirm тоже убираем */
         }
 
-        //load/initialize the osmdroid configuration, this can be done
         val ctx = applicationContext
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
-        //tile servers will get you banned based on this string
-
-        //inflate and create the map
-        //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        // дефолтный сурс это мапник
-        // https://wiki.openstreetmap.org/wiki/Mapnik
+        /* дефолтный сурс тайлов это мапник
+        https://wiki.openstreetmap.org/wiki/Mapnik */
         map = binding.map
         map.setTileSource(TileSourceFactory.MAPNIK)
-        // такое есть, легко и бесплатно
-        //final ITileSource tileSource = TileSourceFactory.USGS_TOPO;
-        //map.setTileSource(tileSource);
+        /* такое тоже есть, легко и бесплатно, зато выглядит плохо и грузится долго */
+        /*final ITileSource tileSource = TileSourceFactory.USGS_TOPO;
+        map.setTileSource(tileSource);*/
 
-        // это для зума двумя пальцами
+        /* это для зума двумя пальцами */
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        // дефолтная локация
+        /* дефолтная локация при загрузке приложения
+        она будет появляться всегда при onCreate, а onCreate появляется при смене активити!
+         */
         val mapController = map.controller
         mapController.setZoom(9.5)
         val startPoint = GeoPoint(48.8583, 2.2944)
         mapController.setCenter(startPoint)
 
-        // тестовый маркер
-        // https://stackoverflow.com/questions/55705988/how-to-add-marker-in-osmdroid
+        /* стартовый тестовый маркер
+        https://stackoverflow.com/questions/55705988/how-to-add-marker-in-osmdroid */
         val testMarker = PinMarker(map)
         testMarker.position = GeoPoint(48.7583, 2.1944)
-        testMarker.icon = pinImage //используем свою картинку
+        testMarker.icon = pinImage
         testMarker.setAnchor(
             Marker.ANCHOR_CENTER,
             Marker.ANCHOR_BOTTOM
-        ) // это вроде просто указывает, какое место
-        // на картинке соответствует указанной координате
+        )
         map.overlays.add(testMarker)
 
-        // всё можно прописать в классе пинмаркер, так что листенер ставить не надо
+        /* это листенер нажатия на маркер
+        всё можно прописать в классе PinMarker, так что листенер ставить не надо */
         /*testMarker.setOnMarkerClickListener { marker, mapView ->
             // делаем интент и вызываем активити по редактировании
             val context = map.context
@@ -147,16 +131,15 @@ class MainActivity : AppCompatActivity() {
             true
         }*/
 
-        // кража координат по тыку + установка маркера -- это не нужно, поскольку установка маркера:
-        // отрисовка перекрестия (ну или чего там) и движение экрана
+        /* получение координат по тыку + установка маркера -- это не нужно, поскольку установка маркера:
+         отрисовка перекрестия (ну или чего там) и движение экрана
+         я это здесь оставил, так как, возможно, оверлеи (для нажатий, например) ещё пригодятся */
         /*val touchOverlay: Overlay = object : Overlay(this) {
             var anotherItemizedIconOverlay: ItemizedIconOverlay<OverlayItem>? = null
 
             override fun draw(arg0: Canvas, arg1: MapView, arg2: Boolean) {}
 
             override fun onSingleTapConfirmed(e: MotionEvent, mapView: MapView): Boolean {
-
-                //TODO only do this if not clicked on a marker
 
                 // по тыку получаем координаты тыка
                 val marker = applicationContext.resources.getDrawable(R.drawable.real_pin)
@@ -176,7 +159,7 @@ class MainActivity : AppCompatActivity() {
                 mapItem.setMarker(marker)
                 overlayArray.add(mapItem)
 
-                // делаем интент и вызываем активити по созданию TODO а остальной код выполнится?
+                // делаем интент и вызываем активити по созданию
                 /*val context = map.context
                 val intent = Intent(context, PinCreateActivity::class.java)
                 context.startActivity(intent)*/
@@ -201,30 +184,22 @@ class MainActivity : AppCompatActivity() {
         map.overlays.add(touchOverlay)*/
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
     override fun onResume() {
         super.onResume()
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        map.onResume() //needed for compass, my location overlays, v6.0.0 and up
+        /*this will refresh the osmdroid configuration on resuming.
+        if you make changes to the configuration, use
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));*/
+        map.onResume() /* needed for compass, my location overlays, v6.0.0 and up */
     }
 
     override fun onPause() {
         super.onPause()
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
-        map.onPause() //needed for compass, my location overlays, v6.0.0 and up
+        /* this will refresh the osmdroid configuration on resuming.
+        if you make changes to the configuration, use
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration.getInstance().save(this, prefs); */
+        map.onPause() /* needed for compass, my location overlays, v6.0.0 and up */
     }
 
     override fun onRequestPermissionsResult(
@@ -233,7 +208,6 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
 
-        // не знаю что это
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         val permissionsToRequest = ArrayList<String>()
         for (i in grantResults.indices) {
@@ -253,7 +227,7 @@ class MainActivity : AppCompatActivity() {
         for (permission in permissions) {
             if (ContextCompat.checkSelfPermission(this, permission)
                 != PackageManager.PERMISSION_GRANTED
-            ) { // разрешения не дали
+            ) {
                 permissionsToRequest.add(permission)
             }
         }
--- app/src/main/java/ru/nsu/ccfit/tsd/pinmap/PinMarker.kt
+++ app/src/main/java/ru/nsu/ccfit/tsd/pinmap/PinMarker.kt
@@ -2,25 +2,20 @@ package ru.nsu.ccfit.tsd.pinmap
 
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 
 class PinMarker : Marker {
 
-    //val lateinit pin: Pin
-
-    constructor(mapView: MapView) : super(mapView) //TODO override it?
+    constructor(mapView: MapView) : super(mapView)
     constructor(mapView: MapView, ctx: Context) : super(mapView, ctx)
 
-    //TODO disable infowindow
-
     override fun onMarkerClickDefault(marker: Marker?, mapView: MapView?): Boolean {
+        /* этот листенер вызывает окно редактирования маркера по нажатию */
         val context = mapView!!.context
         val intent = Intent(context, PinEditActivity::class.java)
         context.startActivity(intent)
-        Toast.makeText(mapView!!.context, "clicked!", Toast.LENGTH_SHORT).show()
-        setInfoWindow(null) // чтобы не показывать эту штуку при тыке
+        setInfoWindow(null) /* это чтобы не показывать InfoWindow при нажатии на маркер */
         return super.onMarkerClickDefault(marker, mapView)
     }
 
