2d6e8e4f9cc62ea942ca7bd7df66ac6a24656ac9
vkharchenko1 <vladimir.khar2@gmail.com>
2023-11-05 13:00:49 +0700

    Replace location with latitude and longitude

--- app/src/main/java/ru/nsu/ccfit/tsd/pinmap/pins/Pin.kt
+++ app/src/main/java/ru/nsu/ccfit/tsd/pinmap/pins/Pin.kt
@@ -5,8 +5,8 @@ import java.util.Date
 class Pin(
     var id: Int,
     var name: String,
-    var location: Int /* TODO: прописать самим локацию или использовать андроидовский класс?
-                            В случае 2 реализованы все возможности, но есть привязка к андроиду */
+    var latitutde: Double,
+    var longitude: Double
 )
 {
     var tags: MutableList<String> = ArrayList() /* TODO: вынести теги в класс или оставить так?
