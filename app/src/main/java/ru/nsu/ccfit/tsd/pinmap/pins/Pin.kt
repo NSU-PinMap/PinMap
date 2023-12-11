8e6c10304435653242d75552e6b375d1337b975e
vkharchenko1 <vladimir.khar2@gmail.com>
2023-12-11 15:21:56 +0700

    В Pin добавлено поле photos: MutableList<Uri>

--- app/src/main/java/ru/nsu/ccfit/tsd/pinmap/pins/Pin.kt
+++ app/src/main/java/ru/nsu/ccfit/tsd/pinmap/pins/Pin.kt
@@ -1,5 +1,6 @@
 package ru.nsu.ccfit.tsd.pinmap.pins
 
+import android.net.Uri
 import java.util.Date
 
 class Pin(
@@ -8,9 +9,8 @@ class Pin(
     var longitude: Double
 ) {
     var id: Int? = null
-    var tags: MutableList<String> = ArrayList() /* TODO: вынести теги в класс или оставить так?
-                                                    В случае 1 можно использовать теги как сущности
-                                                    для поиска в программе */
+    var tags: MutableList<String> = ArrayList()
+    var photos: MutableList<Uri> = ArrayList()
     var description: String = ""
     var mood: UByte = 0u
     var date: Date? = null
